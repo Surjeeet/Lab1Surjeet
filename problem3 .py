2a67d760f44ab9b185bfab146336f0a9b2dcae5e
Jeet13 <Surjeet.Singh3RCE@triosstudent.com>
2023-03-31 11:04:31 -0400

    v3

---
+++ problem3 .py
@@ -0,0 +1,32 @@
+# Define global constants
+PENNY_VALUE = 1
+NICKEL_VALUE = 5
+DIME_VALUE = 10
+QUARTER_VALUE = 25
+PENNIES_IN_DOLLAR = 100
+
+def count_a_dollar():
+    # Take input from user
+    num_pennies = int(input("Enter the number of pennies: "))
+    num_nickels = int(input("Enter the number of nickels: "))
+    num_dimes = int(input("Enter the number of dimes: "))
+    num_quarters = int(input("Enter the number of quarters: "))
+
+    # Calculate total cents
+    total_cents = num_pennies * PENNY_VALUE + num_nickels * NICKEL_VALUE + num_dimes * DIME_VALUE + num_quarters * QUARTER_VALUE
+
+    # Calculate total dollars
+    total_dollars = total_cents / PENNIES_IN_DOLLAR
+
+    # Check if the amount is exactly one dollar
+    if total_dollars == 1.0:
+        print("Congratulations!")
+        print("The amount you entered was exactly one dollar!")
+        print("You win the game!!")
+    elif total_dollars > 1.0:
+        print("Sorry, the amount you entered was more than one dollar.")
+    else:
+        print("Sorry, the amount you entered was less than one dollar.")
+
+# Call the function to play the game
+count_a_dollar()
